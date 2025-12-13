137ddc279dd004971adf2f9de8b4b836c0a6a7df
Seth-Lupo <seth@squibber.com>
2025-12-13 04:26:33 -0500

    chagne

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,12 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : '/kgindustries'
+		}
+	}
 };
 
 export default config;
