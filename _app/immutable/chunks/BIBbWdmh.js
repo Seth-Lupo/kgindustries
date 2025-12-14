9bd0e15ccf900546098a6d9de72b03e1bb428b6b
Seth-Lupo <Seth-Lupo@users.noreply.github.com>
2025-12-14 01:55:53 +0000

    deploy: f2664a3c12eb566b915e846632aaabce109d585c

--- _app/immutable/chunks/BIBbWdmh.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1diovzy?.base??"/kgindustries",e=globalThis.__sveltekit_1diovzy?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/entry/start.BJb_s2Ai.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/HPleM9pT.js";export{o as load_css,r as start};
