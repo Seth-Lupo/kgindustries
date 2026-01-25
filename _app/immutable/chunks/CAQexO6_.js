75c923b9a76fb417dada214c870c1eb3aa9a5693
cooperkole <cooperkole@users.noreply.github.com>
2026-01-25 21:24:53 +0000

    deploy: f96b8a874e10713e0d2869e63f73093382a312f8

--- _app/immutable/chunks/CAQexO6_.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_8qwunx?.base??"/kgindustries",e=globalThis.__sveltekit_8qwunx?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/chunks/j0_h8akv.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_12k8qlb?.base??"/kgindustries",e=globalThis.__sveltekit_12k8qlb?.assets??s??"";export{e as a,s as b};
